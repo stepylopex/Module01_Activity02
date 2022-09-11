8df9fa57cda927ec2b022d6c0ca7311bc6b4faab
Lenovo-PC050\User050 <lopez.stephanie@auf.edu.ph>
2022-09-11 17:01:59 +0800

    Add project files.

---
+++ Module01_Activity02/Module01_Activity02/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Module01_Activity02
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+            CreateCommand = new Command<Type>((Type viewType) =>
+
+            {
+                View view = (View)Activator.CreateInstance(viewType);
+                view.VerticalOptions = LayoutOptions.CenterAndExpand;
+                stackLayout.Children.Add(view);
+            });
+            BindingContext = this;
+        }
+
+        public ICommand CreateCommand { private set; get; }
+
+
+  
+
+    }
+}
---
+++ Module01_Activity02/Module01_Activity02/SharedStatic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Drawing;
+
+
+namespace Module01_Activity02
+{
+    class SharedStatic
+    {
+
+        public static readonly Color ButtonColor = Color.Black;
+        public static readonly Color ButtonTextColor = Color.White;
+     
+
+    }
+}
